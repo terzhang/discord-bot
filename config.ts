7db7c6f77d103dfcfb6aac9dff162808db3e9133
TerZhang <terzhang@hotmail.com>
2020-08-19 05:08:37 -0400

    refactor(index, config): convert to typescript

---
+++ config.ts
@@ -0,0 +1,7 @@
+const config = {
+  admins: ['315850603396071424'], //put the "admins" of the bot here, this will give them access to commands marked with `adminOnly: true`
+  token: 'place-your-token-here', //bot's login token from the discord developer page
+  prefix: '.', //prefix that the command calls should start with
+};
+export default config;
+//any changes to this file will require a full bot restart to update
---
+++ index.ts
@@ -0,0 +1,69 @@
+import discord from 'discord.js';
+import config from './config';
+import fs from 'fs';
+const { prefix, token } = config;
+
+type Commands = discord.Collection<unknown, unknown>;
+type Aliases = discord.Collection<unknown, unknown>;
+type Events = discord.Collection<unknown, unknown>;
+type Functions = discord.Collection<unknown, unknown>;
+
+interface Client extends discord.Client {
+  loadCmds?: (client: any, reload: any) => any;
+  loadFunctions?: (client: any, reload: any) => any;
+  loadEvents?: (client: any, reload: any) => any;
+  commands?: Commands;
+  aliases?: Aliases;
+  events?: Events;
+  functions?: Functions;
+  categories?: string[];
+}
+
+const client: Client = new discord.Client({ disableMentions: 'everyone' }); //remove the parameters being passed into
+
+/**
+ * If you need to use other databases or apis that require credentials,
+ * you should place them here and add them as a property in the bot object
+ */
+
+/**
+ * Tips for database/storage
+ * If you want a local SQL database, I highly recommend sqlite3
+ *      https://www.sqlite.org/index.html https://www.npmjs.com/package/better-sqlite3
+ *      npm i sqlite3 && better-sqlite3
+ *
+ * If you want to use a cloud no-sql database (syncable across multiple instances/bots), I recommend google firestore or mongoDB
+ *      https://firebase.google.com/docs/firestore https://www.mongodb.com/
+ */
+
+let bot = { client, config, prefix };
+/**
+ * These add commands, aliases, events, and functions to the client object
+ * Discord Collections work like a Map/Dictionary Object
+ * To get a specific function from the collection, use
+ * client.xxxxxxx.get("name");
+ */
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+client.events = new discord.Collection();
+client.functions = new discord.Collection();
+client.categories = fs.readdirSync('./commands/');
+
+/**
+ * These functions are used to grab the modules in each corresponding folder
+ * whenever the loadxxx() functions are called, the cache will be cleared of the previous data
+ * and the new data will be loaded into the collections so you can update code without restarting the
+ * entire bot
+ */
+client.loadCmds = (client, reload) =>
+  require(`./handlers/command`)(client, reload);
+client.loadFunctions = (client, reload) =>
+  require(`./handlers/function`)(client, reload);
+client.loadEvents = (client, reload) =>
+  require('./handlers/event.js')(client, reload, bot);
+client.loadCmds(client, false);
+client.loadFunctions(client, false);
+client.loadEvents(client, false);
+
+export default bot;
+client.login(token);
