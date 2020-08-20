e90e834cb3849da5676ecbd1a8d0ffcd0d954e80
TerZhang <terzhang@hotmail.com>
2020-08-20 17:04:57 -0400

    feat: configure bot secrets

--- config.ts
+++ config.ts
@@ -1,6 +1,9 @@
+import { OWNER_ID } from './env/admin.secret';
+import { BOT_TOKEN } from './env/bot.secret';
+
 const config = {
-  admins: ['315850603396071424'], //put the "admins" of the bot here, this will give them access to commands marked with `adminOnly: true`
-  token: 'place-your-token-here', //bot's login token from the discord developer page
+  admins: [OWNER_ID], //put the "admins" of the bot here, this will give them access to commands marked with `adminOnly: true`
+  token: BOT_TOKEN, //bot's login token from the discord developer page
   prefix: '.', //prefix that the command calls should start with
 };
 export default config;
